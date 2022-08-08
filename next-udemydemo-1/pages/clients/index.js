35b6cf9493cccf7ec8022fc6318d58feab594a04
mansinakrani <mansinakrani006@gmail.com>
2022-08-08 14:44:27 +0530

    Navigation with Links

--- next-udemydemo-1/pages/clients/index.js
+++ next-udemydemo-1/pages/clients/index.js
@@ -1,11 +1,25 @@
 // http://localhost:3000/clients
 
+import Link from 'next/link'
+
 function ClientsPage() {
-    return (
-        <div>
-            <h1>The Clients Page</h1>
-        </div>
-    );
+  const clients = [
+    { id: 'max', name: 'Maximilian' },
+    { id: 'manu', name: 'Manuel' },
+  ]
+
+  return (
+    <div>
+      <h1>The Clients Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
-export default ClientsPage;
+export default ClientsPage
