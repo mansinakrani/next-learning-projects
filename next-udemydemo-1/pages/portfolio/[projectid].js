50ef2271d57ab09cc3f6816ae567edebd92cf47a
mansinakrani <mansinakrani006@gmail.com>
2022-08-08 13:18:22 +0530

    Dynamic Routes

--- next-udemydemo-1/pages/portfolio/[projectid].js
+++ next-udemydemo-1/pages/portfolio/[projectid].js
@@ -1,4 +1,12 @@
+import { useRouter } from 'next/router';
 function PortfolioProjectPage() {
+    const router = useRouter();
+    console.log(router.pathname);
+    console.log(router.query);
+
+    //send a request to some backend server
+    //to fetch the piece of data with an id router.query.projectid
+
     return (
         <div>
             <h1>The Portfolio Project Page</h1>
