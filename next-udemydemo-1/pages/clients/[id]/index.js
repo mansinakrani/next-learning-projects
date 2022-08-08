a8de145eaa995eb03ecc556d10e3c4079d04b121
mansinakrani <mansinakrani006@gmail.com>
2022-08-08 15:09:53 +0530

    Programmatic Navigation

--- next-udemydemo-1/pages/clients/[id]/index.js
+++ next-udemydemo-1/pages/clients/[id]/index.js
@@ -2,14 +2,25 @@
 import { useRouter } from 'next/router';
 
 function ClientProjectsPage() {
-    const router = useRouter();
+  const router = useRouter();
 
-    console.log(router.query);
-    return (
-        <div>
-            <h1>The Projects of a Given Client</h1>
-        </div>
-    );
+  console.log(router.query);
+
+  function loadProjectHandler() {
+    // load data...
+    //if we use router.replace , it means we can't go back after the navigation
+    router.push({
+      pathname: '/clients/[id]/[clientprojectid]',
+      query: { id: 'max', clientprojectid: 'projecta' },
+    });
+  }
+
+  return (
+    <div>
+      <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
+    </div>
+  );
 }
 
 export default ClientProjectsPage;
--- next-udemydemo-1/pages/clients/index.js
+++ next-udemydemo-1/pages/clients/index.js
@@ -1,12 +1,12 @@
 // http://localhost:3000/clients
 
-import Link from 'next/link'
+import Link from 'next/link';
 
 function ClientsPage() {
   const clients = [
     { id: 'max', name: 'Maximilian' },
     { id: 'manu', name: 'Manuel' },
-  ]
+  ];
 
   return (
     <div>
@@ -14,12 +14,19 @@ function ClientsPage() {
       <ul>
         {clients.map((client) => (
           <li key={client.id}>
-            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+            <Link
+              href={{
+                pathname: '/clients/[id]',
+                query: { id: client.id },
+              }}
+            >
+              {client.name}
+            </Link>
           </li>
         ))}
       </ul>
     </div>
-  )
+  );
 }
 
-export default ClientsPage
+export default ClientsPage;
