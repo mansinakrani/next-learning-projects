d1ce7c53bae6e54261c57cb9c908cb0f099fa531
Mansi Nakrani <mansinakrani006@gmail.com>
2022-08-16 18:39:20 +0530

    Connecting Components

--- next-udemydemo-section-4/components/events/event-item.js
+++ next-udemydemo-section-4/components/events/event-item.js
@@ -1,6 +1,33 @@
+import Link from 'next/link';
+
 function EventItem(props) {
+    const { title, image, date, location, id } = props;
+    const humanReadableDate = new Date(date).toLocaleTimeString('en-US', {
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric',
+      });
+      const formattedAddress = location.replace(', ', '\n');
+      const exploreLink = `/events/${id}`;
+    
     return (
-        <li></li>
+        <li>
+            <img src={'/' + image} alt={title}/>
+            <div>
+                <div>
+                    <h2>{title}</h2>
+                    <div>
+                        <time>{humanReadableDate}</time>
+                    </div>
+                    <div>
+                        <address>{formattedAddress}</address>
+                    </div>
+                </div>
+                <div>
+                    <Link href={exploreLink}>Explore Event</Link>
+                </div>
+            </div>
+        </li>
     );
 }
 
