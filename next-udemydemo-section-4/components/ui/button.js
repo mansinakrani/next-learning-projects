735054c13e347c295d48f3f4ac893abfd7f17f38
Mansi Nakrani <mansinakrani006@gmail.com>
2022-08-17 18:29:18 +0530

    Adding filter form for filtering events

--- next-udemydemo-section-4/components/ui/button.js
+++ next-udemydemo-section-4/components/ui/button.js
@@ -3,10 +3,18 @@ import Link from "next/link";
 import Styles from './button.module.css';
 
 function Button(props) {
+    if (props.link) {
+        return (
+            <Link href={props.link}>
+                <a className={Styles.btn}>{props.children}</a>
+            </Link>
+        );
+    }
+
     return (
-        <Link href={props.link}>
-            <a className={Styles.btn}>{props.children}</a>
-        </Link>
+        <button className={Styles.btn} onClick={props.onClick}>
+            {props.children}
+        </button>
     );
 }
 
