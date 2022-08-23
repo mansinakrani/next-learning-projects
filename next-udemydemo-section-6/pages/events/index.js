a357a3992c97ebd967aa080f998fbc43b1729a1c
mansinakrani <mansinakrani006@gmail.com>
2022-08-23 17:30:29 +0530

    section-6 Working on all events page

--- next-udemydemo-section-6/pages/events/index.js
+++ next-udemydemo-section-6/pages/events/index.js
@@ -1,12 +1,13 @@
-import { getAllEvents } from "../../dummy-data";
+import { getAllEvents } from "../../helpers/api-util";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
 import { Fragment } from "react";
 import { useRouter } from "next/router";
 
-function AllEventsPage() {
+function AllEventsPage(props) {
   const router = useRouter();
-  const events = getAllEvents();
+  const { events } = props;
+
   function findEventHandler(year, month) {
     const fullPath = `/events/${year}/${month}`;
     router.push(fullPath);
@@ -18,6 +19,16 @@ function AllEventsPage() {
       <EventList items={events} />
     </Fragment>
   );
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events: events
+    }
+  }
 }
 
 export default AllEventsPage;
