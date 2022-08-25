34c70e2948c07deab2a6004f0cd278bfd66838be
mansinakrani <mansinakrani006@gmail.com>
2022-08-25 14:17:49 +0530

    section-7 configuring the head content

--- next-udemydemo-section-6/pages/index.js
+++ next-udemydemo-section-6/pages/index.js
@@ -1,3 +1,5 @@
+import Head from 'next/head';
+
 import { getFeaturedEvents } from '../helpers/api-util';
 import EventList from "../components/events/event-list";
 
@@ -5,6 +7,10 @@ function HomePage(props) {
 
   return (
     <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta name='description' content='Find a lot of great events that allow you to evolve...' />
+      </Head>
       <EventList items={props.events} />
     </div>
   );
