d57d33332923335dc59d3b23c57f18b268959463
mansinakrani <mansinakrani006@gmail.com>
2022-08-29 12:04:32 +0530

    section-7 Optimizing Images with Next Image component

--- next-udemydemo-section-6/components/events/event-item.js
+++ next-udemydemo-section-6/components/events/event-item.js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import AddressIcon from '../icons/address-icon';
 import ArrowRightIcon from '../icons/arrow-right-icon';
 import DateIcon from '../icons/date-icon';
@@ -17,7 +18,7 @@ function EventItem(props) {
     
     return (
         <li className={Styles.item}>
-            <img src={'/' + image} alt={title}/>
+            <Image src={'/' + image} alt={title} width={250} height={160} />            
             <div className={Styles.content}>
                 <div className={Styles.summary}>
                     <h2>{title}</h2>
