794b755e765ad70fb8d4c276cf923e3cfa140af3
mansinakrani <mansinakrani006@gmail.com>
2022-08-31 15:26:46 +0530

    section-8 sending requests to api routes

--- next-udemydemo-section-8/pages/api/feedback.js
+++ next-udemydemo-section-8/pages/api/feedback.js
@@ -1,5 +1,28 @@
+import fs from 'fs'; // file system Node.js module
+import path from 'path'; // path Node.js module
+
 function handler(req, res) {
-    res.status(200).json({ message: 'This works!' });
+    if (req.method === 'POST'){
+        const email = req.body.email;
+        const feedbackText = req.body.text;
+
+        const newFeedback ={ 
+            id: new Date().toISOString(),
+            email: email,
+            text: feedbackText,
+        };
+
+        // store that in a database or in a file
+        const filePath = path.join(process.cwd(), 'data', 'feedback.json');
+        const fileData = fs.readFileSync(filePath); // fs module to write to a file
+        const data = JSON.parse(fileData);
+        data.push(newFeedback);
+        fs.writeFileSync(filePath, JSON.stringify(data));
+        res.status(201).json({ message: 'Success!', feedback: newFeedback });
+    } else {
+        res.status(200).json({ message: 'This works!' });
+    }
+    
 }
 
 export default handler;
