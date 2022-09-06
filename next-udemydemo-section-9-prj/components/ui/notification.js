eaa81412ae2f69680a81f562f04ed5e92e6afa98
mansinakrani <mansinakrani006@gmail.com>
2022-09-06 18:14:18 +0530

    section-10 removing notifications automatically fro newsletter signup

--- next-udemydemo-section-9-prj/components/ui/notification.js
+++ next-udemydemo-section-9-prj/components/ui/notification.js
@@ -1,30 +1,35 @@
-import classes from './notification.module.css'
+import { useContext } from 'react';
+
+import classes from './notification.module.css';
+import NotificationContext from '../../store/notification-context';
 
 function Notification(props) {
-  const { title, message, status } = props
+  const notificationCtx = useContext(NotificationContext);
 
-  let statusClasses = ''
+  const { title, message, status } = props;
+
+  let statusClasses = '';
 
   if (status === 'success') {
-    statusClasses = classes.success
+    statusClasses = classes.success;
   }
 
   if (status === 'error') {
-    statusClasses = classes.error
+    statusClasses = classes.error;
   }
 
   if (status === 'pending') {
-    statusClasses = classes.pending
+    statusClasses = classes.pending;
   }
 
-  const activeClasses = `${classes.notification} ${statusClasses}`
+  const activeClasses = `${classes.notification} ${statusClasses}`;
 
   return (
-    <div className={activeClasses}>
+    <div className={activeClasses} onClick={notificationCtx.hideNotification}>
       <h2>{title}</h2>
       <p>{message}</p>
     </div>
-  )
+  );
 }
 
-export default Notification
+export default Notification;
