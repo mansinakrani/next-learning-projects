f4a435186d82c9a7eb9738ef7f447474a11af42c
mansinakrani <mansinakrani006@gmail.com>
2022-09-08 15:45:26 +0530

    rendered dummy post data

--- next-udemy-full-demo-project/components/posts/post-item.js
+++ next-udemy-full-demo-project/components/posts/post-item.js
@@ -8,13 +8,13 @@ function PostItem(props) {
     const formattedDate = new Date(date).toLocaleDateString('en-US',{ day: 'numeric', month: 'long', year: 'numeric' });
 
     const imagePath = `/images/posts/${slug}/${image}`;
-
+    const linkPath = `/posts/${slug}`;
     return (
         <li className={classes.post}>
-            <Link>
+            <Link href={linkPath}>
                 <a>
                     <div className={classes.image}>
-                        <Image src={imagePath} alt={title} width={300} height={200} />
+                        <Image src={imagePath} alt={title} width={300} height={200} layout='responsive' />
                     </div>
                     <div className={classes.content}>
                         <he>{title}</he>
