88eb09b45719cd2864870a853d7b83a2042de5c2
mansinakrani <mansinakrani006@gmail.com>
2022-09-12 18:35:05 +0530

    adding 'head' data and _document.js file

--- next-udemy-full-demo-project/pages/index.js
+++ next-udemy-full-demo-project/pages/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { Fragment } from "react";
 import FeaturedPosts from "../components/home-page/featured-posts";
 import Profile from "../components/home-page/profile";
@@ -6,6 +7,13 @@ import { getFeaturedPosts } from "../lib/posts-util";
 function HomePage(props) {
   return (
     <Fragment>
+      <Head>
+        <title>Mansi&apos; Blog</title>
+        <meta
+          name='description'
+          content='I post about programming and web development.'
+        />   
+      </Head>
       <Profile />
       <FeaturedPosts posts={props.posts} />    
     </Fragment>
