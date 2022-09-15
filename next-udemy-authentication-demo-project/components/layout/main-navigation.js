59660d2d3eb8bbd1566a281f9958446b7a439011
mansinakrani <mansinakrani006@gmail.com>
2022-09-15 18:11:20 +0530

    managing active session

--- next-udemy-authentication-demo-project/components/layout/main-navigation.js
+++ next-udemy-authentication-demo-project/components/layout/main-navigation.js
@@ -1,8 +1,13 @@
+import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 
 import classes from './main-navigation.module.css';
 
 function MainNavigation() {
+  const { data: session, status } = useSession() //while using useSession add sessionProvider in _app.js file
+  const loading = status === "loading" //v4
+  console.log({ session, loading })
+
   return (
     <header className={classes.header}>
       <Link href='/'>
@@ -12,15 +17,15 @@ function MainNavigation() {
       </Link>
       <nav>
         <ul>
-          <li>
+          {!session && (<li>
             <Link href='/auth'>Login</Link>
-          </li>
-          <li>
+          </li>)}
+          {session && (<li>
             <Link href='/profile'>Profile</Link>
-          </li>
-          <li>
+          </li>)}
+          {session && (<li>
             <button>Logout</button>
-          </li>
+          </li>)}
         </ul>
       </nav>
     </header>
